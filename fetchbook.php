8e9c9e6233526212e0ffba7466d05575fd4aa609
Joanne Marston <joannemarston@gmail.com>
2012-09-20 21:14:15 +0200

    Got bushman story to display - basic
    Edited the fetchbook file to add in html code instead of story file
    containing the html

--- fetchbook.php
+++ fetchbook.php
@@ -1,8 +1,32 @@
 <?php
+$count=1;
+$test=true;
+
+echo "<div id='panel0' class='panel'> <div class='odd'></div><div class='clone even'> <img src='images/01.jpg' /> </div></div>";
+
 $file=fopen($_POST["book"],"r") or exit("Unable to open file!");
-while(!feof($file))
+while($test==true)
 {
-  echo fgets($file);
+	echo "<div id='panel".$count."' class='panel'>";
+	echo "<div class='clone odd'>";
+	
+	for ($i=1; $i<=1500; $i++)
+	{
+		if (!feof($file))
+		{
+			echo fgetc($file);
+		}
+		else
+		{
+			$test=false;
+			break;
+		}
+	}
+	
+	echo "</div>";
+	echo "<div class='clone even'> <img src='images/01.jpg' /> </div>";
+	echo "</div>";
+	$count++;
 }
 
 fclose($file);
